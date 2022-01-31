0ad3d5dd917738a642787a0e7fb37236b6611d07
srikar chavali <chavalisrikar@gmail.com>
2022-01-31 14:11:24 +0000

    added feature to add multiple movies

--- src/app.js
+++ src/app.js
@@ -3,12 +3,17 @@ const {Movie, moviesArray} = require("./utils");
 const app = (args) =>{
     switch(args[2]) {
         case "add":
-            const movie1 = new Movie(args[3], args[4], args[5], args[6])
-            const movie2 = new Movie(args[7], args[8], args[9], args[10])
-            movie1.add();
-            movie2.add()
+            const movie = new Movie(args[3], args[4], args[5], args[6])
+            movie.add();
             console.log(moviesArray);
             break;
+        case "addmulti":
+        const movie1 = new Movie(args[3], args[4], args[5], args[6])
+        const movie2 = new Movie(args[7], args[8], args[9], args[10])
+        movie1.add();
+        movie2.add();
+        console.log(moviesArray);
+        break;
         default:
             console.log("Incorrect command");
     }
